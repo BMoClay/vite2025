0813c6384e9cbca623616f86e09a059b9e4c9c0f
BBen <benmorgancleveland@gmail.com>
2024-11-03 11:54:55 -0500

    first commit to ebshub github

--- src/pages/VaclavEbs.jsx
+++ src/pages/VaclavEbs.jsx
@@ -1,6 +1,13 @@
+import Blurb from "../components/bookBlurb.jsx"
 
 export default function VaclavEbs() {
   return (
+    <>
     <div>VaclavEbs</div>
+    <div className="px-3">
+        <Blurb />
+    </div>
+    </>
+   
   )
 }
